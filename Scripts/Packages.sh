79a99aefa322e0dfb2833b32edcd8b8968005c64
阿战 <xigchn@163.com>
2024-11-01 23:58:36 +0800

    20241101

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -28,8 +28,9 @@ UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "master"
 # UPDATE_PACKAGE "mihomo" "morytyann/OpenWrt-mihomo" "main"
 # UPDATE_PACKAGE "nekoclash" "Thaolga/luci-app-nekoclash" "main"
 # UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
-# UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
+UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
 # UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
+
 
 # UPDATE_PACKAGE "luci-app-advancedplus" "VIKINGYFY/luci-app-advancedplus" "main"
 # UPDATE_PACKAGE "luci-app-gecoosac" "lwb1978/openwrt-gecoosac" "main"
